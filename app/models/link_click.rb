6313f414001c59eaea87ee201b89f6fd1025b5a3
kenyounot123 <kenlu519@gmail.com>
2024-10-05 13:46:24 -0400

    Add model tests for LinkClick

--- app/models/link_click.rb
+++ app/models/link_click.rb
@@ -1,4 +1,17 @@
 class LinkClick < ApplicationRecord
   # query db for basic analytics
   validates :url, presence: true
+
+  def self.total_clicks
+    LinkClick.all.count
+  end
+
+  def self.most_clicked
+    most_clicked_url = LinkClick.group(:url).order("COUNT(id) DESC").limit(1).count(:id).first
+    most_clicked_url[0]
+  end
+
+  def self.active_links
+    LinkClick.group(:url).count
+  end
 end
--- test/models/link_click_test.rb
+++ test/models/link_click_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 class LinkClickTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "invalid link click without url" do
+    no_url_link_click = link_clicks(:no_url)
+    assert_not no_url_link_click.valid?
+  end
+
+  test "#total_clicks should get the total clicks" do
+    count = LinkClick.total_clicks
+    assert_equal LinkClick.count, count
+  end
+
+  test "#most_clicked should get the url and the count of the most clicked url" do
+    unique_urls = LinkClick.group(:url)
+    counts_of_unique_urls = unique_urls.order("COUNT(id) DESC").limit(1).count(:id)
+    assert_equal LinkClick.most_clicked, counts_of_unique_urls.first[0]
+  end
+
+  test "#active_links should give us unique number of urls" do
+    num_of_all_links = LinkClick.group(:url).count
+    assert_equal num_of_all_links, LinkClick.active_links
+  end
 end
